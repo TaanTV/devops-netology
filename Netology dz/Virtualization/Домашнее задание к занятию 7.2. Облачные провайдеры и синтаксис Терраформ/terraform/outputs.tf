c6cefd8fbc554cdba77efe34596a935d0278ffa6
Dmitry Krasilnikov <kras5.59@yandex.ru>
2021-07-23 00:09:34 +0500

    terraform help

---
+++ Netology dz/Virtualization/Домашнее задание к занятию 7.2. Облачные провайдеры и синтаксис Терраформ/terraform/outputs.tf
@@ -0,0 +1,7 @@
+output "instance_ip_addr" {
+  value       = aws_instance.server.private_ip
+  description = "The private IP address of the main server instance."
+  depends_on = [
+    aws_security_group_rule.local_access,
+  ]
+}
