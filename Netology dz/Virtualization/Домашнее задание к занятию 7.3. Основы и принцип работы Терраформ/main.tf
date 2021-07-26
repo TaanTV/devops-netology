9e6b8048e791d19144fdc78544b4d388ba840a07
Dmitry Krasilnikov <kras5.59@yandex.ru>
2021-07-26 17:26:47 +0500

    Push 7.3

---
+++ Netology dz/Virtualization/Домашнее задание к занятию 7.3. Основы и принцип работы Терраформ/main.tf
@@ -0,0 +1,54 @@
+provider "aws" {
+  region = "eu-north-1"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_s3_bucket" "ubuntu" {
+  bucket = "my-tf-test-bucket"
+  acl    = "private"
+}
+
+locals {
+  netology_instance_type_map = {
+    stage = "t2.micro"
+    prod  = "t3.micro"
+  }
+
+  netology_instance_count_map = {
+    stage = 1
+    prod  = 2
+  }
+}
+
+resource "aws_instance" "netology" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = local.netology_instance_type_map[terraform.workspace]
+}
+
+resource "aws_instance" "for_each" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "local.netology_instance_type_map"
+  lifecycle {
+    create_before_destroy = true
+  }
+}
